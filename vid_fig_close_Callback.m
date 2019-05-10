de2ee4a081bb32c782f78938b8af7888abc214fc
Visagan <Shakthi.Visagan@cshs.org>
2019-05-10 15:57:29 -0700

    fixing figure closing

--- vid_fig_close_Callback.m
+++ vid_fig_close_Callback.m
@@ -1,6 +1,6 @@
 function fig_close(hObject,eventdata)
 % Shakthi Visagan
-cd('Z:\New Lab Folder\MATLAB_scripts\NewBoxScripts\START_shakthi_copy\reach_trials_2dot0_SHAKTHI_COPY\videoAnalysisScripts')
+cd('Z:\New Lab Folder\MATLAB_scripts\NewBoxScripts\START_shakthi_copy\reach_trials_2dot0_SHAKTHI_COPY\Reach-Video-Analysis-GUI')
 delete(findobj(0,'type','figure'));  
 clc; clear; close all;
 
